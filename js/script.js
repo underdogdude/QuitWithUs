cd3e463f17a8f6a06c099810c4c0ca5d7a51b974
underdogdude <underdogbright@gmail.com>
2019-11-28 23:04:32 +0800

    add all data before Aon add something new

--- js/script.js
+++ js/script.js
@@ -161,5 +161,97 @@ function showMainDetail(data) {
 function showDetail(data) {
 
 
+    var elems = $("#detail");
+        $(elems).empty();
+
+    var string = `
     
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-title">
+                    <h3>
+                        <i class="fas fa-clipboard-list text__blue"></i> พฤติกรรมการสูบบุหรี่
+                    </h3>
+                    <hr />
+                </div>
+
+                <div class="card-content">
+                    <ul class="list-group list-group-flush detail__list">
+                        <li class="list-group-item">
+                            <b>เริ่มสูบบุหรี่ครั้งแรกเมื่ออายุ</b> : ${ data.startsmoke }
+                        </li>
+                        <li class="list-group-item">
+                            <b>เหตุผล</b> : ${ data.reason }
+                        </li>
+                        <li class="list-group-item">
+                            <b>Often Smoke</b> : ${ data.offensmoke }
+                        </li>
+                        <li class="list-group-item">
+                            <b>When Start</b> : ${ data.whenstart }
+                        </li>
+                        <li class="list-group-item">
+                            <b>After Eating</b> : ${ data.aftereating }
+                        </li>
+                        <li class="list-group-item">
+                            <b>Party</b> : ${ data.party }
+                        </li>
+                        <li class="list-group-item">
+                            <b>Drink</b> : ${data.drink}
+                        </li>
+                        <li class="list-group-item">
+                            <b>Count Smoke</b> : ${data.countsmoke}
+                        </li>
+                        <li class="list-group-item">
+                            <b>Cost</b> : ${data.cost}
+                        </li>
+                        <li class="list-group-item">
+                            <b>Other</b> : ${ data.otherds || "-"}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-title">
+                    <h3>
+                        <i class="fas fa-heartbeat text__blue"></i> โรคประจำตัว
+                    </h3>
+                    <hr />
+                </div>
+
+                <div class="card-content">
+                    <ul class="list-group list-group-flush detail__list">
+                        <li class="list-group-item">
+                            <b>Allergy</b> : ${data.allergy}
+                        </li>
+                        <li class="list-group-item">
+                            <b>Asthma</b> : ${data.asthma}
+                        </li>
+                        <li class="list-group-item">
+                            <b>Blood Pressure</b> : ${data.bloodpressure}
+                        </li>
+                        <li class="list-group-item">
+                            <b>Depress</b> : ${data.depress}
+                        </li>
+                        <li class="list-group-item">
+                            <b>Diabetes</b> : ${data.diabetes}
+                        </li>
+                        <li class="list-group-item">
+                            <b>Disease</b> : ${data.disease}
+                        </li>
+                        <li class="list-group-item">
+                            <b>Dyslipidemia</b> : ${data.dyslipidemia}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    `; 
+    $(elems).append(string);
+    $('html, body').animate({
+        scrollTop: $("#detail_main").offset().top
+    }, 500);
+
 }
