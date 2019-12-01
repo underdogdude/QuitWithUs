7340cf7dfaf7142018281d2c177257af93d33318
underdogdude <underdogbright@gmail.com>
2019-12-01 16:19:19 +0800

    change stype when hover suggestion

--- js/script.js
+++ js/script.js
@@ -346,12 +346,11 @@ function calculateNicotine(level) {
 function analyzeSection(data) { 
 
     var nowTimestamp = Math.floor(Date.now() / 1000);
+    console.log(nowTimestamp);
     var allCountSmoke = ((data.yearsold - data.startsmoke)*365) + (((data.yearsold - data.startsmoke)/4) + 
     ((nowTimestamp - data.timestamp)/86400)) * data.countsmoke;
 
     var allQuitCountSmoke = ( nowTimestamp - data.startquitsmoke ) * data.countsmoke;
-
-
     var stringAnalyze = ` 
         <div class="card mb-4">
             <div class="card-title">
